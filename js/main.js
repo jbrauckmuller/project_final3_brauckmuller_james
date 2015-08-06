33d2b72ec1684b96ec316ef1ebc1243a7de11c36
James Brauckmuller <jamiebrauckmuller@gmail.com>
2015-08-06 12:29:34 -0400

    main js collapsible content divs

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 $( document ).ready(function() {
 
+// Collapsible Content Sections
+	$( '.contact' ).click(function() {
+		$( '.contact_cont' ).slideToggle();
+	});
+
+	$( '.gift_cert' ).click(function() {
+		$( '.gift_cont' ).slideToggle();
+	});
+
+	$( '.award' ).click(function() {
+		$( '.award_cont' ).slideToggle();
+	});
 });
 
 
